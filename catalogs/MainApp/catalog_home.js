02178d890d7f8db5e6e3a99d3729d6ba663050d2
Seonae Gim <seonaegim@Seonaes-iMac.local>
2018-05-02 20:45:24 +0900

    modify steemit fetch script

--- catalogs/MainApp/catalog_home.js
+++ catalogs/MainApp/catalog_home.js
@@ -1,57 +1,62 @@
 include("Scripts/steemjs.js");
 include("config.js");
 
-var last_author   = null;
-var last_permlink = null;
+var account = require("account");
 
-var bundles = [];
+var __last_discussion = null;
 
 function on_loaded() {
-	var start_author = (bundles.length > 0) ? last_author	: null;
-	var start_permlink = (bundles.length > 0) ? last_permlink : null;
+}
+
+function feed_list(keyword, location, length, sortkey, sortorder, handler) {
+	// var username = account.is_logged_in() ? account.username() : "moitto";
+
+	var start_author = (location > 0) ? __last_discussion["author"]	: null;
+	var start_permlink = (location > 0) ? __last_discussion["permlink"] : null;
 
 	steemjs_get_discussions_by_blog(config["tag"], 10, start_author, start_permlink, function(discussions) {
+		var data = [];
+
+		if (location > 0) {
+			discussions = discussions.slice(1);
+		}
+
 		discussions.forEach(function(discussion) {
 			if (!config["author"] || config["author"] === discussion["author"]) {
+				var image_url 		  = __fetch_image_url_in_discussion(discussion);
 				var userpic_url       = __fetch_userpic_url_in_discussion(discussion);
 				var userpic_large_url = __fetch_userpic_large_url_in_discussion(discussion);;
 				var payout_value      = __fetch_payout_value_in_discussion(discussion).toFixed(2);
 
-				var bundle_id = "S_BUNDLES_" + discussion["author"] + "_" + discussion["permlink"];
+				var identifer = "S_FEEDS_" + discussion["author"] + "_" + discussion["permlink"];
 				var title = discussion["title"];
 
 				if (__is_audio_book(title)) {
-					bundles.push({
-            			"id":bundle_id,
-						"author":discussion["author"],
-						"permlink":discussion["permlink"],
-						"title":discussion["title"], 
+					data.push({
+						"id": identifer,
+						"author": discussion["author"],
+						"permlink": discussion["permlink"],
+						"title": __fetch_audio_book_title(title),
+						"number": __fetch_audio_book_number(title),
+						"image-url":image_url,
 						"userpic-url":userpic_url,
 						"userpic-large-url":userpic_large_url,
 						"payout-value":"$" + payout_value.toString(),
+						"votes-count":discussion["net_votes"].toString(),
 						"main-tag":discussion["category"],
             		    "created-at":discussion["created"], 
             		    "hides-navibar":"yes"
-					});
+					})
 				}
 			}
 		});
 
-		last_author = bundles[bundles.length - 1]["author"];
-		last_permlink = bundles[bundles.length - 1]["permlink"];
+		if (discussions.length > 0) {
+			__last_discussion = discussions[discussions.length - 1];
+		}
 
-		__reload_list();
+		handler(data);
 	});
-}
-
-function feed_list(keyword, location, length, sortkey, sortorder, handler) {
-	handler(bundles);
-}
-
-function __reload_list() {
-	var showcase = view.object("showcase.audio");
-
-	showcase.action("reload");
 }
 
 function __is_audio_book(title) {
@@ -64,7 +69,25 @@ function __is_audio_book(title) {
 	return false;
 }
 
+function __fetch_audio_book_number(title) {
+	var num = /\s*오디오북\s*([\d]*)./g.exec(title);
 
+	if (num) {
+		return num[1];
+	}
+
+	return "";
+}
+
+function __fetch_audio_book_title(source_title) {
+	var title = /\s*오디오북\s*[\d]*.\s*(.*)/g.exec(source_title);
+
+	if (title) {
+		return title[1];
+	}
+
+	return "";
+}
 
 function __fetch_payout_value_in_discussion(discussion) {
 	var total_payout_value = parseFloat(discussion["total_payout_value"].replace("SBD", "").trim());
@@ -82,6 +105,16 @@ function __fetch_userpic_url_in_discussion(discussion) {
 
 function __fetch_userpic_large_url_in_discussion(discussion) {
 	return "https://steemitimages.com/u/" + discussion["author"] + "/avatar";
+}
+
+function __fetch_image_url_in_discussion(discussion) {
+	var images = JSON.parse(discussion["json_metadata"])["image"];
+
+	if (images && images.length > 0) {
+		return images[0];
+	}
+
+	return "";
 }
 
 function __fetch_image_url_in_content(content) {
